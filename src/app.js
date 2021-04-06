dc0a931e953d96bf3d9b0b681d0717aeec12d2d5
ARobin2288 <79115206+ARobin2288@users.noreply.github.com>
2021-04-06 11:20:04 -0500

    adding functions

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 function displayTemperature(response) {
-    console.log(response.data.main.temp);
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let locationElement = document.querySelector("#location");
+    let conditionsElement = document.querySelector("#weather-conditions")
+    let humidityElement = document.querySelector("#humidity");
+    let feelsElement = document.querySelector("#feels-temp");
+    let sunriseElement = document.querySelector("#sunrise-time");
+    let sunsetElement = document.querySelector("#sunset-time");
+    temperatureElement.innerHTML = Math.round (response.data.main.temp);
+    locationElement.innerHTML = response.data.name;
+    conditionsElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    feelsElement.innerHTML = Math.round (response.data.main.feels_like);
+    sunriseElement.innerHTML = response.data.sys.sunrise;
+    sunsetElement.innerHTML = response.data.sys.sunset;
 }
 
 let apiKey = "0f1996bbebf340db45987ce9fc344036";
